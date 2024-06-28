fcb4a1c71ff761c2f23a7d4e24976bbfb35172d5
Mikeymoo5 <moodids@outlook.com>
2024-06-28 15:53:09 -0500

    Continue Gymnasium environment development

--- src/neuralnetgame/Environment/Environment.py
+++ src/neuralnetgame/Environment/Environment.py
@@ -20,25 +20,49 @@ class SimEnvironment(gym.Env):
         self.cell_size = cell_size
         self.grid_size = grid_size
 
-        self._action_to_str = {
-            0: "nothing",
-            1: "up",
-            2: "down",
-            3: "left",
-            4: "right",
-            5: "interact"
+        self._action_map = {
+            0: [0, 0], # Nothing
+            1: [0, -1], # Up
+            2: [0, 1], # Down
+            3: [-1,0], # Left
+            4: [1, 0], # Right
+            5: "interact" # Interact
         }
     def _move_pet(self, move: list):
-        pass #TODO Accept an array, add it to the pet's location, and check for bounds
+        new_loc = np.array(self._pet["loc"]) + np.array(move)
+        if new_loc < 0 or new_loc >= self.grid_size or new_loc == self._food_loc or new_loc == self._water_loc:
+            return {
+                "action_type": "move",
+                "status": "fail",
+                "loc": self._pet["loc"]
+            }
+        else:
+            return {
+                "action_type": "move",
+                "status": "success",
+                "loc": new_loc
+            }
+
     def _interact_pet(self):
-        pass #TODO Check if the pet is near a food or water tile, then update stats accordingly
+        #TODO: re-write to only do required calculations to optimize interactions
+        food_dist = np.linalg.norm(np.array(self._pet["loc"]) - np.array(self._food_loc))
+        water_dist = np.linalg.norm(np.array(self._pet["loc"]) - np.array(self._water_loc))
+        if food_dist < 1:
+            self._pet["hunger"] = np.minimum(self._pet["hunger"] + 20, 100)
+            return {"action_type": "interact", "status": "success", "interact_type": "food"}
+        elif water_dist < 1:
+            self._pet["thirst"] = np.minimum(self._pet["thirst"] + 20, 100)
+            return {"action_type": "interact", "status": "success", "interact_type": "water"}
+        else:
+            return {"action_type": "interact", "status": "fail", "interact_type": "none"}
+        
     def _get_obs(self):
         return {
             "world": self._world,
             "pet": {
-                "loc": self._pet_loc,
-                "hunger": self._pet_hunger,
-                "thirst": self._pet_thirst
+                "loc": self._pet["loc"],
+                "hunger": self._pet["hunger"],
+                "thirst": self._pet["thirst"]
             }
         }
     
@@ -47,15 +71,44 @@ class SimEnvironment(gym.Env):
             "temp": "temp"
         }
     
+    # Calculate the reward
+    def _get_reward(self, action):
+        reward = 0
+
+        # Punish the agent for dying and immediately return
+        if self._pet["hunger"] <= 0 or self._pet["thirst"] <= 0:
+            reward = -1000
+            return reward
+        
+        # Reward the agent for eating or drinking when under 80 hunger or thirst
+        if action["action_type"] == "interact":
+            if action["status"] == "success":
+                if action["interact_type"] == "food":
+                    if self._pet["hunger"] <= 80:
+                        reward += 3
+                if action["interact_type"] == "water":
+                    if self._pet["thirst"] <= 80:
+                        reward += 3
+
+        # Punish the agent for low hunger or thirst
+        if self._pet["hunger"] <= 40:
+            reward -= 2
+        if self._pet["thirst"] <= 40:
+            reward -= 2
+        
+        return reward
+    
     def _render_frame(self):
         pass #TODO: Render the current state of the environment
 
     def reset(self, seed=None, options=None):
         super().reset(seed=seed)
         self._world = np.zeros((self.grid_size, self.grid_size), dtype=int)
-        self._pet_loc = [self.grid_size//2, (self.grid_size//4)*3]
-        self._pet_hunger = 100
-        self._pet_thirst = 100
+        self._pet = {
+            "loc": [self.grid_size//2, (self.grid_size//4)*3],
+            "hunger": 100,
+            "thirst": 100
+        }
 
         self._food_loc = [self.grid_size//4, self.grid_size//4]
         self._water_loc = [(self.grid_size//4)*3, self.grid_size//4]
@@ -66,28 +119,27 @@ class SimEnvironment(gym.Env):
         return observation, info
     
     def step(self, action):
-        act_str = self._action_to_str[action]
+        # Convert an action into a dict {type, status, loc/none}
+        action = self._action_map[action]
+        if action is list:
+            self.action = self._move_pet(action)
+            self._pet["loc"] = action
+        elif action == "interact":
+            self.action = self._interact_pet()
+        else:
+            # Anything that isnt a list and isnt a string is invalid - how did we get here?
+            raise ValueError("Invalid action - Achievement unlocked: How did we get here?")
+        # Decrease hunger and thirst every step
+        self._pet["hunger"] -= 0.75
+        self._pet["thirst"] -= 0.75
 
-        # TODO: Find a more efficient way to do this
-        if act_str == "nothing":
-            act_type = 0
-            move = np.array([0, 0])
-        elif act_str == "up":
-            act_type = 0
-            move = np.array([0, -1])
-        elif act_str == "down":
-            act_type = 0
-            move = np.array([0, 1])
-        elif act_str == "left":
-            act_type = 0
-            move = np.array([-1, 0])
-        elif act_str == "right":
-            act_type = 0
-            move = np.array([1, 0])
-        elif act_str == "interact":
-            act_type = 1
+        observation = self._get_obs()
+        info = self._get_info()
+        reward = self._get_reward(action)
+        terminated = False
 
-        if act_type == 0:
-            self._move_pet(move)
-        elif act_type == 1:
-            self._interact_pet()
+        # Check if the pet is dead
+        if self._pet["hunger"] <= 0 or self._pet["thirst"] <= 0:
+            terminated = True
+
+        return observation, reward, terminated, info
